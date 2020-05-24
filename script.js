a89d036c54d9d7373947ba9d2bc6d0ca386919cb
Balou van Iterson <balouviterson@gmail.com>
2020-05-24 23:45:26 +0200

    Final
    Key-animaties toegevoegd
    Responsive
    Effecten toegevoegd

---
+++ script.js
@@ -0,0 +1,29 @@
+/* Keyboard */
+
+var body = document.querySelector("body");
+console.log(body);
+
+window.addEventListener("keydown", toggle);
+
+function toggle(event) {
+    if (event.keyCode === 66) {
+        body.classList.toggle("blauw");
+    }
+
+    if (event.keyCode === 71) {
+        body.classList.toggle("geel");
+    }
+
+    if (event.keyCode === 32) {
+        body.classList.toggle("donker");
+    }
+
+    if (event.keyCode === 80) {
+        body.classList.toggle("party");
+    }
+}
+
+/* Audio */
+
+var audio = document.getElementById("music");
+audio.volume = 0.2;
